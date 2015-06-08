048c755e53a3cede3911021692a88f9081cd55d4
Oasis Vali <oasis.vali@gmail.com>
2015-06-08 11:23:18 +0530

    added custom secret key for qa server

--- hwcentral/prod_settings.py
+++ hwcentral/prod_settings.py
@@ -4,5 +4,9 @@ from settings import *
 # This useless assert statement simply so pycharm doesnt mark the above import as unused and remove it while optimizing
 assert SECRET_KEY
 
+# prod secret key should only be on prod server
+with open('/etc/secret_key.txt') as f:
+    SECRET_KEY = f.read().strip()
+
 # no debug for us
 DEBUG = False
