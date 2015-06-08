754ba8405e2540c0ad7c1ba1add02f24d12e6b06
Oasis Vali <oasis.vali@gmail.com>
2015-06-08 10:45:53 +0530

    fixed entire settings import being marked as unused for prod_settings

--- hwcentral/prod_settings.py
+++ hwcentral/prod_settings.py
@@ -1,4 +1,8 @@
 # pull in the normal settings
+from settings import *
+
+# This useless assert statement simply so pycharm doesnt mark the above import as unused and remove it while optimizing
+assert SECRET_KEY
 
 # no debug for us
 DEBUG = False
