79caf1a9b06372b9c70d634950685c166a837ca1
Oasis Vali <oasis.vali@gmail.com>
2015-06-15 17:25:31 +0530

    removed ambiguous wording

--- devops/run-gunicorn.sh
+++ devops/run-gunicorn.sh
@@ -6,6 +6,7 @@ export WORKON_HOME=/home/oasis/.virtualenvs
 source /usr/local/bin/virtualenvwrapper.sh
 
 workon hwcentral
+# exec is needed otherwise supervisor just supervises this script and not the forked gunicorn processes
 exec gunicorn hwcentral.wsgi -c devops/gunicorn_conf.py
 
 
