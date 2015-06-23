9ca44fe64d8d2462f1029dd6d0f0e334a627fc2d
Sharang Pai <sharangpai123@gmail.com>
2015-06-23 09:56:56 +0530

    changes to student home

--- core/static/js/charts/mockdata.js
+++ core/static/js/charts/mockdata.js
@@ -41,292 +41,248 @@ var mock={
                 subject:"Science",
                 student_average:82,
                 class_average:77,
-            }
+            },
             {
                 subject:"Social Studies",
                 student_average:88,
                 class_average:81,
-            }
+            },
             {
                 subject:"English",
                 student_average:91,
                 class_average:88,
-            }
+            },
             {
                 subject:"Second Language",
                 student_average:95,
                 class_average:97,
-            }
+            },
             {
                 subject:"FIT",
                 student_average:84,
                 class_average:89,
             }
         ]   
-    }
+    },
 
     breakdown_listing:[
         {
             subject:"Mathematics",
-            subject_teacher:"Sandeep Raut",
+            subject_teacher:"Anjali Lal",
             listing:[
                 {
                     date: "July 18",
                     topic: "Complex Numbers",
                     class_average: 81,
                     student_score: 85
-                }
+                },
                 {
                     date: "July 19",
                     topic: "Series and Sequences",
                     class_average: 84,
                     student_score: 86
-                }
+                },
                 {
                     date: "July 20",
                     topic: "Integration",
                     class_average: 94,
                     student_score: 96
-                }
+                },
                 {
                     date: "July 21",
                     topic: "3D Geometry",
                     class_average: 95,
                     student_score: 91
-                }
+                },
                 {
                     date: "July 23",
                     topic: "Probability",
                     class_average: 77,
                     student_score: 82
                 }
-                
-        }
+            ]    
+        },
         {
             subject:"Science",
-            subject_teacher:"Sandeep Raut",
+            subject_teacher:"Amita Singh",
             listing:[
                 {
                     date: "July 18",
-                    topic: "Complex Numbers",
-                    class_average: 81,
-                    student_score: 85
-                }
+                    topic: "Organic Chemistry",
+                    class_average: 89,
+                    student_score: 83
+                },
                 {
                     date: "July 19",
-                    topic: "Series and Sequences",
-                    class_average: 84,
-                    student_score: 86
-                }
-                {
-                    date: "July 20",
-                    topic: "Integration",
-                    class_average: 94,
-                    student_score: 96
-                }
+                    topic: "Atoms and Molecules",
+                    class_average: 83,
+                    student_score: 28
+                },
                 {
                     date: "July 21",
-                    topic: "3D Geometry",
-                    class_average: 95,
+                    topic: "Redox Reactions",
+                    class_average: 74,
                     student_score: 91
-                }
+                },
                 {
-                    date: "July 23",
-                    topic: "Probability",
-                    class_average: 77,
-                    student_score: 82
+                    date: "July 22",
+                    topic: "Nuclear Chemistry",
+                    class_average: 87,
+                    student_score: 99
+                },
+                {
+                    date: "July 24",
+                    topic: "Environmental Chemistry",
+                    class_average: 81,
+                    student_score: 77
                 }
-                
-        }
+            ]    
+        },
         {
-            subject:"Mathematics",
-            subject_teacher:"Sandeep Raut",
+            subject:"Social Studies",
+            subject_teacher:"Rama Kumar",
             listing:[
                 {
-                    date: "July 18",
-                    topic: "Complex Numbers",
-                    class_average: 81,
-                    student_score: 85
-                }
+                    date: "July 17",
+                    topic: "India Geography",
+                    class_average: 89,
+                    student_score: 82
+                },
                 {
                     date: "July 19",
-                    topic: "Series and Sequences",
-                    class_average: 84,
-                    student_score: 86
-                }
+                    topic: "Pre-independent India",
+                    class_average: 70,
+                    student_score: 88
+                },
                 {
                     date: "July 20",
-                    topic: "Integration",
-                    class_average: 94,
-                    student_score: 96
-                }
+                    topic: "Politics in India",
+                    class_average: 83,
+                    student_score: 95
+                },
                 {
-                    date: "July 21",
-                    topic: "3D Geometry",
-                    class_average: 95,
-                    student_score: 91
-                }
+                    date: "July 20",
+                    topic: "Natural Resources",
+                    class_average: 92,
+                    student_score: 97
+                },
                 {
                     date: "July 23",
-                    topic: "Probability",
-                    class_average: 77,
-                    student_score: 82
+                    topic: "Books of the Past",
+                    class_average: 84,
+                    student_score: 94
                 }
-                
-        }
+            ]   
+        },
         {
-            subject:"Mathematics",
-            subject_teacher:"Sandeep Raut",
+            subject:"English",
+            subject_teacher:"Shivani Lal",
             listing:[
                 {
+                    date: "July 16",
+                    topic: "Ghost of Canterville",
+                    class_average: 87,
+                    student_score: 91
+                },
+                {
                     date: "July 18",
-                    topic: "Complex Numbers",
-                    class_average: 81,
-                    student_score: 85
-                }
+                    topic: "Tale of Two cities",
+                    class_average: 82,
+                    student_score: 88
+                },
                 {
                     date: "July 19",
-                    topic: "Series and Sequences",
-                    class_average: 84,
-                    student_score: 86
-                }
-                {
-                    date: "July 20",
-                    topic: "Integration",
+                    topic: "Ranga's Marriage",
                     class_average: 94,
                     student_score: 96
-                }
+                },
                 {
                     date: "July 21",
-                    topic: "3D Geometry",
-                    class_average: 95,
-                    student_score: 91
-                }
+                    topic: "Wolf Grey",
+                    class_average: 89,
+                    student_score: 100
+                },
                 {
                     date: "July 23",
-                    topic: "Probability",
-                    class_average: 77,
-                    student_score: 82
+                    topic: "Lorum Ipsum",
+                    class_average: 71,
+                    student_score: 72
                 }
-                
-        }
+            ]    
+        },
         {
-            subject:"Mathematics",
-            subject_teacher:"Sandeep Raut",
+            subject:"Second Language",
+            subject_teacher:"Swagata Rudra",
             listing:[
                 {
                     date: "July 18",
-                    topic: "Complex Numbers",
-                    class_average: 81,
-                    student_score: 85
-                }
+                    topic: "Shlokas",
+                    class_average: 74,
+                    student_score: 99
+                },
                 {
                     date: "July 19",
-                    topic: "Series and Sequences",
+                    topic: "Vedas",
                     class_average: 84,
-                    student_score: 86
-                }
+                    student_score: 80
+                },
                 {
                     date: "July 20",
-                    topic: "Integration",
-                    class_average: 94,
+                    topic: "Yajurveda",
+                    class_average: 87,
+                    student_score: 92
+                },
+                {
+                    date: "July 20",
+                    topic: "Atharvaveda",
+                    class_average: 93,
+                    student_score: 90
+                },
+                {
+                    date: "July 25",
+                    topic: "Sandhi",
+                    class_average: 92,
                     student_score: 96
                 }
-                {
-                    date: "July 21",
-                    topic: "3D Geometry",
-                    class_average: 95,
-                    student_score: 91
-                }
-                {
-                    date: "July 23",
-                    topic: "Probability",
-                    class_average: 77,
-                    student_score: 82
-                }
-                
-        }
+            ]   
+        },
         {
-            subject:"Mathematics",
+            subject:"FIT",
             subject_teacher:"Sandeep Raut",
             listing:[
                 {
-                    date: "July 18",
-                    topic: "Complex Numbers",
-                    class_average: 81,
-                    student_score: 85
-                }
+                    date: "July 17",
+                    topic: "Binary Representation",
+                    class_average: 95,
+                    student_score: 99
+                },
                 {
                     date: "July 19",
-                    topic: "Series and Sequences",
-                    class_average: 84,
-                    student_score: 86
-                }
+                    topic: "Basics of HTML",
+                    class_average: 10,
+                    student_score: 100
+                },
                 {
                     date: "July 20",
-                    topic: "Integration",
-                    class_average: 94,
-                    student_score: 96
-                }
+                    topic: "Basics of XML",
+                    class_average: 73,
+                    student_score: 82
+                },
                 {
                     date: "July 21",
-                    topic: "3D Geometry",
-                    class_average: 95,
-                    student_score: 91
-                }
+                    topic: "JSON introduction",
+                    class_average: 87,
+                    student_score: 76
+                },
                 {
-                    date: "July 23",
-                    topic: "Probability",
-                    class_average: 77,
-                    student_score: 82
+                    date: "July 25",
+                    topic: "Concepts of Javascript",
+                    class_average: 82,
+                    student_score: 77
                 }
-                
+            ]
         }
-        {
-            subject:"Mathematics",
-            subject_teacher:"Sandeep Raut",
-            listing:[
-                {
-                    date: "July 18",
-                    topic: "Complex Numbers",
-                    class_average: 81,
-                    student_score: 85
-                }
-                {
-                    date: "July 19",
-                    topic: "Series and Sequences",
-                    class_average: 84,
-                    student_score: 86
-                }
-                {
-                    date: "July 20",
-                    topic: "Integration",
-                    class_average: 94,
-                    student_score: 96
-                }
-                {
-                    date: "July 21",
-                    topic: "3D Geometry",
-                    class_average: 95,
-                    student_score: 91
-                }
-                {
-                    date: "July 23",
-                    topic: "Probability",
-                    class_average: 77,
-                    student_score: 82
-                }
-                
-        }
-
-
-
-        
-
-
-
-
-
+    ]
 }
 var test=new Performance(mock);
 
--- core/static/js/subjectbar.js
+++ core/static/js/subjectbar.js
@@ -1,12 +1,16 @@
 $(document).ready(function () {
-	$('ul.nav-tabs li a').click(function (e) {
-  		$('ul.nav-tabs li.active').removeClass('active')
-  		$(this).parent('li').addClass('active')
-})
-$('.chart').hide();
-$('#linegraph1').show();
- $('.sub').click(function(){
-              $('.chart').hide();
-              $('#linegraph'+$(this).attr('target')).show();
-        });
-})
+	$('.chart').hide();
+    $('#linegraph1').show();
+    $('.sub').click(function(){
+        $('.chart').hide();
+        $('#linegraph'+$(this).attr('target')).show();
+    });
+    for (var i=0;i<test.breakdown_listing.length;i++){
+        $("#subjectbar").append(
+            "<li class=sub> <a>"+test.breakdown_listing[i].subject+"</a></li> ");
+        }
+    $('ul.nav-tabs li a').click(function (e) {
+        $('ul.nav-tabs li.active').removeClass('active');
+        $(this).parent('li').addClass('active');
+    }); 
+});
