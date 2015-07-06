dfd844641b275c7fab8a7d08486148930ab5694d
hrishikesh_rao <hrishikeshrao94@gmail.com>
2015-07-06 18:25:26 +0530

    check1

--- core/view_models/announcement.py
+++ core/view_models/announcement.py
@@ -5,7 +5,3 @@ class AnnouncementBody(FormBody):
     """
     Abstract class that is used to store the form for the Announcement creation views
     """
-
-    def __init__(self,form):
-        self.form=form
-        pass
