d9a91ee698ebe4cad1f7f34ae3b075a6312327a3
Sharang Pai <sharangpai123@gmail.com>
2015-07-10 10:48:53 +0530

    admin and parent fixes

--- core/static/js/common.js
+++ core/static/js/common.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
-    $('#announcement_table').dataTable();
+    $('#announcement_table').dataTable({
+        'order':[[2]]
+    });
     $('#active_assignment_table').dataTable();
     $('#graded_assignment_table').dataTable();
 });
