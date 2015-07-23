b72d851c88a1127c70b0d3017f7f9b3e0709dfb9
hrishikesh_rao <hrishikeshrao94@gmail.com>
2015-07-23 13:19:26 +0530

    added validator,changed comments,trello todo for frontend

--- core/forms/assignment.py
+++ core/forms/assignment.py
@@ -5,7 +5,7 @@ class AssignmentForm(forms.Form):
     def __init__(self,teacher,*args,**kwargs):
         super(AssignmentForm,self).__init__(*args,**kwargs)
         aql_options_list =[]
-        separator = "_"
+        SEPARATOR = "_"
         self.fields['subjectroom'] =forms.ModelChoiceField(queryset=SubjectRoom.objects.filter(teacher=teacher))
         for subjectroom in SubjectRoom.objects.filter(teacher__id=teacher.pk):
             subject_id =subjectroom.subject_id
@@ -13,7 +13,7 @@ class AssignmentForm(forms.Form):
             standard_id = ClassRoom.objects.get(id=classroom_id).standard_id
             for assignmentql in AssignmentQuestionsList.objects.filter(subject_id=subject_id,standard_id=standard_id):
                 aql_options_list.append(
-                    (separator.join([str(standard_id), str(subject_id), assignmentql.description, str(assignmentql.pk)]),
+                    (SEPARATOR.join([str(standard_id), str(subject_id), assignmentql.description, str(assignmentql.pk)]),
                      str(assignmentql)))
 
         #options list format : standardid_subjectid_description;assignemntsubject-assignmentnumber
