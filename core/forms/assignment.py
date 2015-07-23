ef3af248fb80ffdc048866c977a3b75a6b8cc11d
hrishikesh_rao <hrishikeshrao94@gmail.com>
2015-07-23 16:55:03 +0530

    phwww... working finally

--- core/forms/assignment.py
+++ core/forms/assignment.py
@@ -4,20 +4,22 @@ import django
 from django.forms.widgets import SplitDateTimeWidget
 from core.models import  SubjectRoom, AssignmentQuestionsList, ClassRoom
 class AssignmentForm(forms.Form):
+    SEPARATOR = "_"
     def __init__(self,teacher,*args,**kwargs):
         super(AssignmentForm,self).__init__(*args,**kwargs)
         aql_options_list =[]
-        SEPARATOR = "_"
+
         self.fields['subjectroom'] =forms.ModelChoiceField(queryset=SubjectRoom.objects.filter(teacher=teacher))
         for subjectroom in teacher.subjects_managed_set.all():
             subject =subjectroom.subject
             standard = ClassRoom.objects.get(pk=subjectroom.classRoom.pk).standard
             for assignmentql in AssignmentQuestionsList.objects.filter(subject=subject,standard=standard,school=teacher.userinfo.school):
                 aql_options_list.append(
+                    #These will be te ID
                     (SEPARATOR.join([str(standard.pk), str(subject.pk), assignmentql.description, str(assignmentql.pk)]),
+                     #This will be the display
                      str(assignmentql)))
 
-        #options list format : standardid_subjectid_description;assignemntsubject-assignmentnumber
         self.fields['question sets'] = forms.ChoiceField(choices=aql_options_list)
 
     assigned = forms.DateTimeField(widget=SplitDateTimeWidget())
@@ -34,13 +36,13 @@ class AssignmentForm(forms.Form):
         due = cleaned_data['due']
         now = django.utils.timezone.now()
 
-        if  assigned < now-datetime.timedelta(seconds = SECONDS_BUFFER):
+        if  (assigned) < (now-datetime.timedelta(seconds = SECONDS_BUFFER)):
             raise forms.ValidationError("An assigment cannot be assigned in the past. ")
 
-        if due<assigned:
+        if (due)<assigned:
             raise forms.ValidationError(" An assignment cant be due before it has been assigned! ")
 
-        if due< now + datetime.timedelta(days=ASSIGNMENT_MIN_ACTIVE_DAYS):
+        if (due)< now + datetime.timedelta(days=ASSIGNMENT_MIN_ACTIVE_DAYS):
             raise forms.ValidationError(" Every assignment must be open for at least a day. ")
 
 
--- core/view_drivers/assignment.py
+++ core/view_drivers/assignment.py
@@ -2,34 +2,12 @@ from django.http import HttpResponseNotFound
 from django.shortcuts import render, redirect
 from core.forms.assignment import AssignmentForm
 from core.models import Assignment, AssignmentQuestionsList, SubjectRoom
-from django  import forms
-import datetime
 from core.routing.urlnames import UrlNames
 from core.view_drivers.base import GroupDrivenViewCommonTemplate
 from core.view_models.assignment import AssignmentBody
 from core.view_models.base import AuthenticatedBase
 from core.view_models.sidebar import TeacherSidebar
 
-
-def validate_assignment(form):
-        assigned = form.cleaned_data['assigned']
-        due = form.cleaned_data['due']
-        assigned_date = assigned.date()
-        assigned_time = assigned.time()
-        due_date = due.date()
-        due_time = due.time()
-        today = datetime.date.today()
-        now = datetime.datetime.now()
-        if  assigned_date != today:
-            raise forms.ValidationError("The assigment must be assigned today! ")
-
-        if assigned_time > (now-datetime.timedelta(seconds=70)).time():
-            raise forms.ValidationError(" The time when assigment is assigned is invalid! ")
-        if due_date<assigned_date:
-            raise forms.ValidationError(" An assignment cant be due before it has been assigned! ")
-        if due_date< today + datetime.timedelta(1):
-            raise forms.ValidationError(" Every assignment must at least have a day before it's due ")
-        return
 
 class AssignmentDriver(GroupDrivenViewCommonTemplate):
     def __init__(self, request):
@@ -50,23 +28,23 @@ class AssignmentGet(AssignmentDriver):
     def teacher_endpoint(self):
         # form to create assignment from assignmentquestionslist
         form = AssignmentForm(self.user)
-        return render(self.request, UrlNames.ASSIGNMENT.get_template(),AuthenticatedBase(TeacherSidebar(self.user),AssignmentBody(form))
+        return render(self.request, self.template,AuthenticatedBase(TeacherSidebar(self.user),AssignmentBody(form))
                       .as_context() )
 
 
 class AssignmentPost(AssignmentDriver):
+    REDIRECT_TARGET = UrlNames.HOME.name
     def teacher_endpoint(self):
         # form to create assignment from assignmentquestionslist
         form = AssignmentForm(self.user,self.request.POST)
-        # validate_assignment(form)
         if form.is_valid():
-            aqllist= form.cleaned_data['question sets'].split('_')
-            assignmentQuestionsList = AssignmentQuestionsList.objects.get(id=aqllist[3])
-            subjectRoom = SubjectRoom.objects.get(id =form.cleaned_data['subjectroom'].id)
+            aql= form.cleaned_data['question sets'].split(AssignmentForm.SEPARATOR)[-1]
+            assignmentQuestionsList = AssignmentQuestionsList.objects.get(pk=aql)
+            subjectRoom = SubjectRoom.objects.get(pk =form.cleaned_data['subjectroom'].pk)
             assigned = form.cleaned_data['assigned']
             due = form.cleaned_data['due']
             Assignment.objects.create(assignmentQuestionsList=assignmentQuestionsList,subjectRoom=subjectRoom,assigned=assigned,due=due)
-            return redirect(UrlNames.HOME.name)
+            return redirect(AssignmentPost.REDIRECT_TARGET)
         else:
-            return render(self.request, UrlNames.ASSIGNMENT.get_template(),
+            return render(self.request, self.template,
                           AuthenticatedBase(TeacherSidebar(self.user),AssignmentBody(form)).as_context() )
