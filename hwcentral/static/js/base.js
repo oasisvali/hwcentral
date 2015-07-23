e5d21a3965f230e27b49fdb9a6769cf7bcfab4ba
Sharang Pai <sharangpai123@gmail.com>
2015-07-23 15:53:05 +0530

    mobile sniffing

--- hwcentral/static/js/base.js
+++ hwcentral/static/js/base.js
@@ -2,3 +2,7 @@ $(document).ready(function () {
     $("select").addClass("chosen-select");
     $(".chosen-select").chosen()
 });
+
+if (screen.width<=760){
+    alert ("Sorry ! HWCentral does not support this device. To ensure an optimal experience, try logging in from a non-mobile device")
+}
