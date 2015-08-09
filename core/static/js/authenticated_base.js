863c638840066bf30b08d0ddedec87806d749882
Sharang Pai <sharangpai123@gmail.com>
2015-08-09 13:07:50 +0530

    date time picker widget

--- core/static/js/authenticated_base.js
+++ core/static/js/authenticated_base.js
@@ -1,13 +1,35 @@
 var DATATABLES_DEBUG=false;
 var CHART_ENDPOINT="http://localhost:8000/chart/";
-var MIN_DIMENSION=992;
+var MIN_DIMENSION=600;
 if (screen.width<=MIN_DIMENSION || screen.height<=MIN_DIMENSION){
-    alert ("Sorry ! HWCentral does not support this device. To ensure an optimal experience, try logging in from a non-mobile device"+"screen-width:"+
-            screen.width+"screen-height:"screen.height);
+    alert ("Sorry ! HWCentral does not support this device. To ensure an optimal experience, try logging in from a non-mobile device");
     window.stop();
 }
 
 $(document).ready(function () {
+    $(function(){
+       $(".date_picker").pickadate({
+
+            format: 'dd/mm/yyyy',
+            monthSelector: false,
+            yearSelector: false,
+            onStart: function(){
+                this.set('select',Date.now())
+            },
+            min: Date.now(),
+        });
+        $(".time_picker").pickatime({
+
+            format: 'HH:i',
+            disable: [
+                true,
+                [20,0],
+            ],
+            interval: 240,
+
+            
+        });
+    });
     $(document).tooltip();
     $('#announcement_table').dataTable({
         "pagingType":"full_numbers"
