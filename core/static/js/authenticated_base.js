9a37b92a2e9a829ff4d704c30573016ba6f6e8ec
Sharang Pai <sharangpai123@gmail.com>
2015-08-12 16:25:15 +0530

    success modal right now some more integration with backend is required but basic setup is there you can play around with it

--- core/static/js/authenticated_base.js
+++ core/static/js/authenticated_base.js
@@ -1,9 +1,14 @@
 var DATATABLES_DEBUG=false;
+var MODAL_SHOW=false;
 var CHART_ENDPOINT="http://localhost:8000/chart/";
 var MIN_DIMENSION=600;
 if (screen.width<=MIN_DIMENSION || screen.height<=MIN_DIMENSION){
     alert ("Sorry ! HWCentral does not support this device. To ensure an optimal experience, try logging in from a non-mobile device");
     window.stop();
+}
+
+if (MODAL_SHOW===true){
+  $("#success_message").modal('show');
 }
 
 $(document).ready(function () {
