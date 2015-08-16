a16e72bd8af6d41cbdea4dbdd7b68fcc863d5722
Sharang Pai <sharangpai123@gmail.com>
2015-08-16 11:09:13 +0530

    changed date_time_picker to one seperate js file

--- core/static/js/authenticated_base.js
+++ core/static/js/authenticated_base.js
@@ -12,63 +12,6 @@ $(document).ready(function () {
       e.preventDefault();
     });
 
-
-    if($("#date_assigner").length){
-      $(function(){
-         $("#assigned_date").pickadate({
-
-              format: 'dd/mm/yyyy',
-              monthSelector: false, // prevents drop downs for month and year picker
-              yearSelector: false,
-              onStart: function(){    // sets default date as current date
-                  this.set('select',Date.now())
-              },
-              min: Date.now(), // prevents user from selecting past dates
-          });
-         $("#submission_date").pickadate({
-
-              format: 'dd/mm/yyyy',
-              monthSelector: false,
-              yearSelector: false,
-          });
-
-
-
-
-          var from_input = $('#assigned_date').pickadate();
-          var assigned_picker = from_input.pickadate('picker');
-
-          var to_input = $('#submission_date').pickadate();
-          var submission_picker = to_input.pickadate('picker');
-
-
-
-          // Check if there’s a “from” date to start with.
-          if (assigned_picker.get('value') ) {
-            submission_picker.set('min', assigned_picker.get('select'));
-          }
-
-          // When something is selected, update the "submission” limits.
-          assigned_picker.on('set', function(event) {
-            if ( event.select ) {
-              submission_picker.set('min', assigned_picker.get('select'));    
-            }
-            else if ( 'clear' in event ) {
-              submission_picker.set('min', false);
-            }
-          })
-      
-          $(".time_picker").pickatime({
-            format: "HH:i", // puts format in 24 hour clock 
-            interval: 240,// sets interval to every 4 hours
-            disable:[ // disables all values except one for selection
-              true,
-              [20,0],
-            ],
-          });
-      });
-    }
-
     if($("#success_message").length){
       $("#success_modal").modal('show');
     }
