4af004d329c4d20ae0481f7b6dac9e2171e0e16c
Oasis Vali <oasis.vali@gmail.com>
2015-09-09 00:22:40 +0530

    prod db password now secure

--- hwcentral/settings.py
+++ hwcentral/settings.py
@@ -51,13 +51,14 @@ MANAGERS = ADMINS
 
 if DEBUG:
     DB_NAME = 'hwcentral-dev'
+    DB_PASSWORD = 'hwcentral'
 
 else:
     DB_NAME = 'hwcentral-qa'
-
+    with open(os.path.join(PROD_CONFIG_ROOT, 'db_password.txt'), 'r') as f:
+        DB_PASSWORD = f.read().strip()
 
 DB_USER = 'root'
-DB_PASSWORD = 'hwcentral'
 # signifies localhost
 DB_HOST = ''
 DB_PORT = ''
