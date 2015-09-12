b80c157294862a96a2f791dc6a18ee3501cb8e0f
Oasis Vali <oasis.vali@gmail.com>
2015-09-12 16:52:33 +0530

    devops fixes

--- devops/deploy.sh
+++ devops/deploy.sh
@@ -2,12 +2,13 @@
 
 # Usage: devops/deploy.sh (from hwcentral root dir)
 
+sudo nginx -s stop
 sudo supervisorctl stop gunicorn
 git pull origin master
 scripts/collab/virtualenv_cleanup.sh
 devops/prep-deploy.sh
-nginx -s reload
 # allow log files to be created inside devops directory
 chmod 777 devops/
 sudo supervisorctl update gunicorn
 sudo supervisorctl start gunicorn
+sudo nginx
