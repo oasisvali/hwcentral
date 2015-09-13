9595f07d3f931eef264cd37d5b2a1ff3a72e2381
Oasis Vali <oasis.vali@gmail.com>
2015-09-13 17:53:40 +0530

    removed fabric datadog integration

--- fabfile.py
+++ fabfile.py
@@ -1,33 +1,25 @@
-from fabric.decorators import task
+from fabric.decorators import hosts, task
 from fabric.operations import run
 from fabric.state import env
-
-from dogapi.fab import setup, notify
-
-setup("11ef54b9111e793be921e57768d67906")
 
 WEB_SERVERS = ['119.9.77.38']
 DB_SERVERS = ['119.9.88.54']
 env.forward_agent = True
 env.port = 1463
 
-
-@notify
 @task
+@hosts(WEB_SERVERS + DB_SERVERS)
 def deploy():
-    env.hosts = WEB_SERVERS + DB_SERVERS
     run("devops/deploy.sh")
 
 
-@notify
 @task
+@hosts(WEB_SERVERS)
 def sleep():
-    env.hosts = WEB_SERVERS
     run("devops/sleep-mode-on.sh")
 
 
-@notify
 @task
+@hosts(WEB_SERVERS)
 def wake():
-    env.hosts = WEB_SERVERS
     run("devops/sleep-mode-off.sh")
