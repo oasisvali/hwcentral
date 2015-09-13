49ad945fdadce1b77d978c48e8f77fdbf10232b6
Oasis Vali <oasis.vali@gmail.com>
2015-09-13 12:34:36 +0530

    updated gunicorn user to oasis, truncating with sudo for nginx

--- devops/truncate-logs.sh
+++ devops/truncate-logs.sh
@@ -2,7 +2,7 @@
 
 # Usage: devops/truncate-logs.sh (from hwcentral root dir)
 
-truncate devops/nginx_access.log --size=0
-truncate devops/nginx_error.log --size=0
+sudo truncate devops/nginx_access.log --size=0
+sudo truncate devops/nginx_error.log --size=0
 truncate devops/gunicorn_access.log --size=0
 truncate devops/gunicorn_stderr.log --size=0
