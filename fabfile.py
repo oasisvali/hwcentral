54af586f81996c10610040346cb4e15e4298f81b
Oasis Vali <oasis.vali@gmail.com>
2015-09-14 13:14:44 +0530

    more fabfile tasks

--- fabfile.py
+++ fabfile.py
@@ -3,15 +3,20 @@ from fabric.operations import run
 from fabric.state import env
 
 WEB_SERVERS = ['119.9.77.38']
-DB_SERVERS = ['119.9.88.54']
+DB_SERVER = '119.9.88.54'
 env.forward_agent = True
 env.port = 1463
 
 @task
-@hosts(WEB_SERVERS + DB_SERVERS)
+@hosts(WEB_SERVERS + [DB_SERVER])
 def deploy():
     run("devops/deploy.sh")
 
+
+@task
+@hosts(WEB_SERVERS + [DB_SERVER])
+def dd_restart():
+    run("sudo /etc/init.d/datadog-agent restart")
 
 @task
 @hosts(WEB_SERVERS)
@@ -23,3 +28,15 @@ def sleep():
 @hosts(WEB_SERVERS)
 def wake():
     run("devops/sleep-mode-off.sh")
+
+
+@task
+@hosts(WEB_SERVERS[0])
+def grade():
+    run("./manage.py runscript scripts.grade_overnight -v3")
+
+
+@task
+@hosts(DB_SERVER)
+def backup():
+    run("devops/backup.sh")
