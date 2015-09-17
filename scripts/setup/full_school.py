ca4da8e5c90ff2f7d470f177f1ede1c87d903449
Oasis Vali <oasis.vali@gmail.com>
2015-09-18 03:33:56 +0530

    fixed circleci setup password issue

--- scripts/setup/full_school.py
+++ scripts/setup/full_school.py
@@ -32,7 +32,7 @@ SUBJECTROOM_CSV_PATH = os.path.join(DATA_DIR, 'subjectroom.csv')
 
 DEBUG_SETUP_PASSWORD = "gKBuiGurx9k2j7BDIq5JYkkamK4"
 
-if settings.DEBUG:
+if settings.DEBUG or settings.CIRCLECI:
     SETUP_PASSWORD = DEBUG_SETUP_PASSWORD
 else:
     with open(os.path.join(settings.PROD_CONFIG_ROOT, 'setup_password.txt'), 'r') as f:
