c897eb999b72d1a83c6571199bd6305e34297c0b
Sharang Pai <sharangpai123@gmail.com>
2015-10-05 19:58:51 +0530

    glitchy save button

--- core/static/js/charm.js
+++ core/static/js/charm.js
@@ -12,5 +12,17 @@ $(document).ready(function () {
             }, scroll_top_duration
         );
     });
+});
 
+$(window).scroll(function(){
+  var footerTopPos = $('#footer').offset().top;
+  var savebuttonBottomPos = $('.charm').offset().top + $('.charm').outerHeight();
+
+  if(savebuttonBottomPos >= footerTopPos) {
+    $('.charm').css('position', 'absolute');
+  }
+
+  else{
+    $('.charm').css ('position','fixed');
+  }
 });
