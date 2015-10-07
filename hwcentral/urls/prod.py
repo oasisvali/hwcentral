f9ba9507059e075b864550410181fc04a8b91813
Oasis Vali <oasis.vali@gmail.com>
2015-10-07 12:15:26 +0530

    exposing admin for prod

--- hwcentral/urls/prod.py
+++ hwcentral/urls/prod.py
@@ -4,6 +4,7 @@ from core.routing.routers import dynamic_router
 from core.utils.constants import HttpMethod
 from core.views import test_500_get
 from hwcentral.urls.common import get_all_env_urlpatterns
+from hwcentral.urls.debug import get_admin_urlpatterns
 
 urlpatterns = get_all_env_urlpatterns() + get_admin_urlpatterns() + [
     # add a error page view to test error page
