de5f3fd0318e7646ee0387cb20ad0010fd92630d
Oasis Vali <oasis.vali@gmail.com>
2015-10-07 12:13:20 +0530

    exposing admin for prod

--- hwcentral/urls/debug.py
+++ hwcentral/urls/debug.py
@@ -9,17 +9,24 @@ from sphinx.views import deal_subpart_post, download_json_get
 
 
 def get_debug_urlpatterns():
-    return get_all_env_urlpatterns() + [
+    return get_all_env_urlpatterns() + get_admin_urlpatterns() + get_sphinx_urlpatterns()
+
+
+def get_admin_urlpatterns():
+    return [
         url('^admin/doc/', include('django.contrib.admindocs.urls')),
         url('^admin/', include(admin.site.urls)),
-    ] + [
-               url(SphinxUrlNames.INDEX.url_matcher, static_csrf_cookie_router, {'template': 'app.html'},
-                   name=SphinxUrlNames.INDEX.name),
-               url(SphinxUrlNames.DEAL_SUBPART.url_matcher, dynamic_router, {HttpMethod.POST: deal_subpart_post},
-                   name=SphinxUrlNames.DEAL_SUBPART.name),
+    ]
+
+
+def get_sphinx_urlpatterns():
+    return [
+        url(SphinxUrlNames.INDEX.url_matcher, static_csrf_cookie_router, {'template': 'app.html'},
+            name=SphinxUrlNames.INDEX.name),
+        url(SphinxUrlNames.DEAL_SUBPART.url_matcher, dynamic_router, {HttpMethod.POST: deal_subpart_post},
+            name=SphinxUrlNames.DEAL_SUBPART.name),
         url(SphinxUrlNames.DOWNLOAD_JSON.url_matcher, dynamic_router, {HttpMethod.GET: download_json_get},
             name=SphinxUrlNames.DOWNLOAD_JSON.name),
     ]
-
 
 urlpatterns = get_debug_urlpatterns()
--- hwcentral/urls/prod.py
+++ hwcentral/urls/prod.py
@@ -5,7 +5,7 @@ from core.utils.constants import HttpMethod
 from core.views import test_500_get
 from hwcentral.urls.common import get_all_env_urlpatterns
 
-urlpatterns = get_all_env_urlpatterns() + [
+urlpatterns = get_all_env_urlpatterns() + get_admin_urlpatterns() + [
     # add a error page view to test error page
     url('^tTrNJnEzCxJfqtDBtWO2cOo6dsA/', dynamic_router, {HttpMethod.GET: test_500_get})
 ]
