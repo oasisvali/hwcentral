aa414ac7fe28bad5f2a5947cf6f234c81cf43646
Oasis Vali <oasis.vali@gmail.com>
2015-10-11 00:09:14 +0530

    removed cron file truncation

--- devops/truncate-logs.sh
+++ devops/truncate-logs.sh
@@ -6,4 +6,4 @@ truncate devops/nginx_access.log --size=0
 truncate devops/nginx_error.log --size=0
 truncate devops/gunicorn_access.log --size=0
 sudo truncate devops/gunicorn_stderr.log --size=0
-truncate devops/cron.log --size=0
+#truncate devops/cron.log --size=0  # dont truncate cron file log, truncating it seems to break the loggly integration
