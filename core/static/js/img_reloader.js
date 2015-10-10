9ce5503ffa088cf8a3ed8b9e68d25acb4c199ab9
Oasis Vali <oasis.vali@gmail.com>
2015-10-10 23:19:01 +0530

    added log message in img_reloader

--- core/static/js/img_reloader.js
+++ core/static/js/img_reloader.js
@@ -3,6 +3,7 @@ function img_reload(image) {
     IMG_RELOAD_RETRIES -= 1;
     if (IMG_RELOAD_RETRIES > 0) {
         setTimeout(function () {
+            console.log('reloading image');
             image.src = image.src;  // forces the browser to hit the src again
         }, 1000 + (Math.random() * 2000));
     }
