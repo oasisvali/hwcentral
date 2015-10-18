786ad81e2a352a8df5acfa6479e0bd6afac63d97
Oasis Vali <oasis.vali@gmail.com>
2015-10-18 17:42:09 +0530

    moved docker stuff out

--- docker/cabinet/run-cabinet.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# clone the cabinet repo (only sanity test branch is needed for now)
-git clone -b sanity_test git@github.com:sidhantp/hwcentral-cabinet.git
-
-cd hwcentral-cabinet
-
-# run nginx
-scripts/docker-init.sh
