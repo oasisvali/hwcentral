2f0aed58d00edac607a3125d3bec9b1df2a8d6a9
Oasis Vali <oasis.vali@gmail.com>
2016-01-09 11:02:03 +0530

    changed downtime, admin home table fix

--- core/static/js/authenticated/home/admin.js
+++ core/static/js/authenticated/home/admin.js
@@ -1,6 +1,13 @@
 $(document).ready(function(){
     $('#classrooms_table .classrooms_table_classroom').click(function(){
         $(this).nextUntil('#classrooms_table .classrooms_table_classroom').slideToggle(500);
-        $(this).toggleClass("open");
+        $(this).toggleClass("expanded");
+        // if any classroom rows are open, mark the entire table as open
+        if ($("#classrooms_table .classrooms_table_classroom.expanded").length > 0) {
+            $("#classrooms_table").addClass("open");
+        }
+        else {
+            $("#classrooms_table").removeClass("open");
+        }
     });
 });
