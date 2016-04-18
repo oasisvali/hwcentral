bf285dfadbe385fea2249ec71a9cc1f5430a3ede
Oasis Vali <oasis.vali@gmail.com>
2016-04-18 11:43:53 +0530

    easier practice for QA

--- core/view_models/ajax.py
+++ core/view_models/ajax.py
@@ -5,9 +5,12 @@ from django.db.models import Q
 from django.utils.html import escape
 
 from core.models import AssignmentQuestionsList, Chapter, Submission
+from core.utils.constants import HWCentralEnv
 from core.utils.json import JSONModel
 from core.utils.labels import get_date_label
 from core.utils.references import HWCentralRepo
+from hwcentral.exceptions import InvalidHWCentralEnvError
+from hwcentral.settings import ENVIRON
 
 
 class AnnouncementRow(JSONModel):
@@ -25,21 +28,39 @@ class StudentSubjectRoomSelectElem(SubjectRoomSelectElem):
     def __init__(self, student, subjectroom):
         chapters = []
 
-        accessible_aqls = defaultdict(set)
-        for submission in Submission.objects.filter(
-                assignment__subjectRoom=subjectroom,
-                student=student,
-                assignment__due__lte=django.utils.timezone.now()
-        ):
-            # this aql has already been assigned to the student i.e. it is accessible
-            aql = submission.assignment.assignmentQuestionsList
-            accessible_aqls[aql.chapter.pk].add(aql.pk)
+        if ENVIRON == HWCentralEnv.PROD or ENVIRON == HWCentralEnv.CIRCLECI:
+            accessible_aqls = defaultdict(set)
+            for submission in Submission.objects.filter(
+                    assignment__subjectRoom=subjectroom,
+                    student=student,
+                    assignment__due__lte=django.utils.timezone.now()
+            ):
+                # this aql has already been assigned to the student i.e. it is accessible
+                aql = submission.assignment.assignmentQuestionsList
+                accessible_aqls[aql.chapter.pk].add(aql.pk)
 
-        for chapter_id, aql_set in accessible_aqls.iteritems():
-            chapter = Chapter.objects.get(pk=chapter_id)
-            aqls = AssignmentQuestionsList.objects.filter(pk__in=list(aql_set)).order_by('number')
+            for chapter_id, aql_set in accessible_aqls.iteritems():
+                chapter = Chapter.objects.get(pk=chapter_id)
+                aqls = AssignmentQuestionsList.objects.filter(pk__in=list(aql_set)).order_by('number')
 
-            chapters.append(ChapterSelectElem(chapter, aqls))
+                chapters.append(ChapterSelectElem(chapter, aqls))
+
+        elif ENVIRON == HWCentralEnv.QA or ENVIRON == HWCentralEnv.LOCAL:
+            # allow student to practice all available assignments - hence similar aql selection logic as teacher
+            school_filter = Q(school=subjectroom.classRoom.school) | Q(school=HWCentralRepo.refs.SCHOOL)
+            standard_subject_filter = Q(subject=subjectroom.subject) & Q(standard=subjectroom.classRoom.standard)
+
+            aql_query = school_filter & standard_subject_filter
+
+            for chapter_id in AssignmentQuestionsList.objects.filter(aql_query).values_list("chapter",
+                                                                                            flat=True):
+                chapter = Chapter.objects.get(pk=chapter_id)
+                aqls = AssignmentQuestionsList.objects.filter(aql_query, chapter=chapter).order_by('number')
+                chapters.append(ChapterSelectElem(chapter, aqls))
+
+
+            else:
+                raise InvalidHWCentralEnvError(ENVIRON)
 
         super(StudentSubjectRoomSelectElem, self).__init__(subjectroom, chapters)
 
