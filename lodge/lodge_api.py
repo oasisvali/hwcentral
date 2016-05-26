89e6a2177e6c2b827181d32b3ef5a257d50c7727
Oasis Vali <oasis.vali@gmail.com>
2016-05-26 15:17:29 +0530

    new video

--- lodge/lodge_api.py
+++ lodge/lodge_api.py
@@ -1,7 +1,5 @@
-from django.shortcuts import get_object_or_404
-
 from lodge.models import Video
 
 
 def get_video_uri(pk):
-    return get_object_or_404(Video, pk=pk).url
+    return Video.objects.get(pk=pk).url
